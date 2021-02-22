7cd510d0eac02e6d5aa1675e8294324e2b279e31
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-02-22 22:04:52 +0545

    add material ui package

---
+++ todo-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo-App</h1>
+      <input/>
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default App;
