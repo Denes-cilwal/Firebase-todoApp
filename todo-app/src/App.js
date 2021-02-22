dc80600a4782305e00a1925737bc8c767f676116
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-02-22 22:21:42 +0545

    add data to todo list

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[todos, setTodos] = useState(['Lets go for outing','I am very fond of playing futsal'])
+  const[input,setInput] = useState('');
+
+  const addTodosHandler = (event)=>{
+    //prevant from refreshing page on adding todos
+    event.preventDefault();
+    console.log("console1",input)
+    // appending new data along with new data ...(spread operator)...concept 
+    setTodos([...todos,input])
+    // clear input field after adding todos in the list
+    setInput('');
+  }
   return (
     <div className="App">
       <h1>Todo-App</h1>
-      <input/>
-      <button>Submit</button>
+      <form>      {/* onchange captures every event trigger */}
+      <input value={input} onChange={(event)=>{setInput(event.target.value)}}/>
+      <button type="submit" onClick={addTodosHandler}>Submit</button>
+     </form>
+      <ul>
+        {/* map array todos with every element todo */}
+        {todos.map(todo =>(
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
