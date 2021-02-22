96b7a3cb21d7481866c36a32aa7976b93bdb352f
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-02-22 23:36:51 +0545

    add list components and react jss package

--- todo-app/src/Components/todo.js
+++ todo-app/src/Components/todo.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import { List, ListItem, ListItemText } from "@material-ui/core";
+import React from "react";
 
-export const Todo = (props)=> {
-    return (
-        <div>
-        <li> {props.todo}</li>
-        </div>
-    )
-}
-
+export const Todo = (props) => {
+  return (
+    <div>
+      <List>
+        <ListItem>
+          <ListItemText primary={props.todo} secondary="DummyText" />
+        </ListItem>
+      </List>
+    </div>
+  );
+};
