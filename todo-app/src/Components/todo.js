6bbd77205fae02622fa361a5e525d4a764117c7a
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-02-23 10:38:57 +0545

    app connect to firebase

--- todo-app/src/Components/todo.js
+++ todo-app/src/Components/todo.js
@@ -1,10 +1,19 @@
 import { List, ListItem, ListItemText } from "@material-ui/core";
 import React from "react";
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  alignlist:{
+    textAlign:"center"
+  }
+});
 
 export const Todo = (props) => {
+  const classes = useStyles();
+
   return (
     <div>
-      <List>
+      <List className={classes.alignlist}>
         <ListItem>
           <ListItemText primary={props.todo} secondary="DummyText" />
         </ListItem>
