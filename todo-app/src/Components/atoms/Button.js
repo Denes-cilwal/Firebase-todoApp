274f9d56fd2cb31d43dd4d330641bf4eca27aab2
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-02-23 09:54:24 +0545

    format code

--- todo-app/src/Components/atoms/Button.js
+++ todo-app/src/Components/atoms/Button.js
@@ -6,7 +6,12 @@ import Button from '@material-ui/core/Button';
 export const ButtonComponent = (props) =>{
 return(
     <div>
-        <Button disabled={!props.input} variant="contained" color="primary" type="submit" onClick={props.addTodosHandler}>
+        <Button 
+        disabled={!props.input} 
+        variant="contained" 
+        color="primary" 
+        type="submit" 
+        onClick={props.addTodosHandler}>
         Submit-Todo
         </Button>
     </div>
